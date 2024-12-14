1358b72993f10cce6e1baf239d919544ec9abaa9
maaya <huki.j37@gmail.com>
2024-12-14 16:34:24 +0900

    アプリエンドポイントの追加

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,19 @@
 from flask import Flask
 from flask import request
-import flask
+import agent
 app = Flask(__name__)
+
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    # post bodyをjson型で取得
+    data = request.get_json()
+
+    # agentからのメッセージを取得
+    #response_message = agent.orchastrator(data)
+
+
+
 
 @app.route('/', methods=['GET'])
 def hello():
