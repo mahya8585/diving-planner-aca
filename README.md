8ce411c12093ed99ef184a0392dd38c97e0f3deb
maaya <huki.j37@gmail.com>
2024-12-15 13:50:52 +0900

    WIP 追加

--- README.md
+++ README.md
@@ -1,14 +1,4 @@
 # diving-planner-aca
 
 
-SET RESOURCE_GROUP="album-containerapps"
-SET LOCATION="canadacentral"
-SET ENVIRONMENT="env-album-containerapps"
-SET API_NAME="album-api"
-SET GITHUB_USERNAME="mahya8585"
-SET ACR_NAME="acaalbums"$GITHUB_USERNAME
-
-https://github.com/mahya8585/containerapps-albumapi-python.git
-
-az containerapp up --name %API_NAME% --resource-group %RESOURCE_GROUP% --location %LOCATION% --environment %ENVIRONMENT% --context-path ./src --repo https://github.com/mahya8585/containerapps-albumap-python
 
--- app/agent.py
+++ app/agent.py
@@ -1,7 +1,8 @@
+import requests
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
-
-def agent_general(request_message: str):
+def call_agent_general(request_message: str):
     """
     汎用エージェント。素のAOAIで回答を生成する。
     :param request_message: 入力メッセージ
@@ -12,6 +13,16 @@ def agent_general(request_message: str):
 
 
     return ''
+
+def call_agent_multi(request_message: str, agent_name: str):
+    """
+    各種エージェントの呼び出し。
+    1 functions app内のエンドポイントを呼び分ける想定
+    :param request_message: リクエストメッセージ
+    :return: response_message: 応答メッセージ
+    """
+    response_message = requests.post(agent_name, json={"message": request_message})
+    return response_message.json()
 
 
 def operate(message: str):
@@ -35,10 +46,25 @@ def response(request_message: str):
     """
 
     # オペレータエージェント使って呼び出すエージェントを判定させる
-    api_names = operate(request_message)
+    agent_names = operate(request_message)
 
     # エージェントが見つからなかった場合は汎用エージェントを使って返答を作成する
-    if len(api_names) == 0:
-        return 'すみません、その質問にはお答えできません'
+    if len(agent_names) == 0:
+        return call_agent_general(request_message)
 
-    return ''
+    # リストにあるAPIに対して並列呼び出しをおこなう
+    responses = []
+    with ThreadPoolExecutor():
+        future_to_api = {ThreadPoolExecutor().submit(
+            call_agent_multi, request_message, agent_name): agent_name for agent_name in agent_names}
+
+        for future in as_completed(future_to_api):
+            api_name = future_to_api[future]
+            try:
+                data = future.result()
+                responses.append(data)
+            except Exception as exc:
+                print(f'{api_name} generated an exception: {exc}')
+
+    # TODO: ここで土のメッセージがだれの応答メッセージなのかを明記したデータを生成する
+    return responses
